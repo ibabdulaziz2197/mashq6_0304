f407e22556e4d4943a4adc57459c9cab4630209c
Abdulaziz <ibabdulaziz2197@gmail.com>
2026-04-03 16:22:36 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# 5
+a= [-2, 0, 5, 8, -1]
+natija = list(filter(lambda x: x > 0, a))
+print(natija)
+
+
+# 6
+a = [1, 6, 3, 8, 2, 10]
+natija = list(filter(lambda x: x > 5, a))
+print(natija)
+
+
+# 7
+a= [12, 5, 7, 20, 3, 15]
+natija = list(filter(lambda x: x < 10, sonlar3))
+print(natija)
+
+# 8
+a = [3, 5, 9, 10, 12, 14]
+natija = list(filter(lambda x: x % 3 == 0, a))
+print(natija)
+
+# 9
+a = [6, 8, 12, 15, 18, 20]
+natija = list(filter(lambda x: x % 2 == 0 and x % 3 == 0, a))
+print(natija)
+
+
+# 10
+a = [0, 1, 2, 0, 3, 0, 4]
+natija = list(filter(lambda x: x != 0, a))
+print(natija)
